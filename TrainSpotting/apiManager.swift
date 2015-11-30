084231dc7327b3999313631b3024c08a430e443d
Jormatosijorma <joonas.maliniemi@gmail.com>
2015-11-30 14:28:36 +0200

    hgwiuorhpgiw

---
+++ TrainSpotting/apiManager.swift
@@ -0,0 +1,28 @@
+//
+//  apiManager.swift
+//  JSONJoy
+//
+//  Created by Tuukka Ojala on 30/11/15.
+//  Copyright © 2015 Vluxe. All rights reserved.
+//
+
+import Foundation
+import JSONJoy
+
+class ApiManager {
+    static let baseUrl = "htp://rata.digitraffic.fi/api/v1"
+
+    class func httpGet(path: String, onComplete: (NSData, NSError) -> Void) {
+        let url =  NSURL(string: baseUrl + path)
+        let request = NSMutableURLRequest(URL: url!)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request, completionHandler: { data, response, error in onComplete(data!, error!)
+        })
+        task.resume()
+    }
+
+    class func getSchedule(departureDate: String, departureStation: String, arrivalStation: String, onComplete: (Train?, NSError?) -> Void) {
+        httpGet("/schedule?departure_date=\(departureDate)&departure_station=\(departureStation)&arrival_station=\(arrivalStation)", onComplete: { data, error in onComplete(Train(JSONDecoder(data)), error)
+        })
+    }
+}
